41210d8e4bd435471e3d0407e3d66cf970d678ea
Michael <96025706+CorruptChoco@users.noreply.github.com>
2022-03-26 19:47:28 -0400

    adding multiple plot folder

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -6,18 +6,22 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color:"black",
-    fillColor:"yellow",
- }).addTo(map);
-
-// another option that uses pixels as "radius" instead of meters
-// L.circleMarker([34.0522, -118.2437]{
+// L.circle([34.0522, -118.2437], {
 //     radius: 300,
 //     color:"black",
 //     fillColor:"yellow",
-// }).addTo(map);
+//  }).addTo(map);
+
+// Get data from cities.js
+let cityData=cities;
+
+  // Loop through the cities array and create one marker for each city.
+    cityData.forEach(function(city) {
+    //console.log(city)
+    L.circleMarker(city.location, {radius: city.population/100000})
+    .bindPopup("<h2>"+city.city+","+city.state+"</h2><hr><h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
